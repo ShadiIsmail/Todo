48bb465606e0b63db53c59682f809342e8033381
Shadi <shadi28eg@gmail.com>
2017-10-21 01:14:55 +0200

    Initial commit

---
+++ assets/js/todos.js
@@ -0,0 +1,25 @@
+$("ul").on("click" , "li" , function(){
+	$(this).toggleClass("complete");
+});
+
+// click on x to delete
+
+$("ul").on("click" ,"span" , function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input").on("keypress",function(event){
+	if(event.which === 13){
+	var todoText = $(this).val();
+	$(this).val("");
+	$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+
+	}
+});
+
+$(".fa-pencil").on("click",function(){
+	$("input").fadeToggle();
+	});
